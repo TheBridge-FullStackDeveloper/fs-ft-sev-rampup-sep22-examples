5109dab1c48aaafd2e719ff6c1213ebc90b70757
Antonio Llamas <antonio.llamas@thebridgeschool.es>
2022-11-11 13:14:28 +0100

    fix express

--- core/week7/day2/backend/app.js
+++ core/week7/day2/backend/app.js
@@ -16,7 +16,4 @@ app.get('/', (req, res) => {
     res.send("<h1>soy root!!!</h1>")
 })
 
-
-
-
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
