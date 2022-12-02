9ccb1aa425c511348f081a3121e5e71b1396cc85
Antonio Llamas <antonio.llamas@thebridgeschool.es>
2022-12-02 13:04:48 +0100

    fix server

--- core/week10/day5/server/src/startup/db.js
+++ core/week10/day5/server/src/startup/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 module.exports = function () {
     const db = process.env.MONGO_URI
+
     mongoose.connect(db)
     .then(() => winston.info("Conectado a mongodb..."))
 }
